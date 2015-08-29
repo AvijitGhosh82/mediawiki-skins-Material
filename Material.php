8d5684b52a93a0cbccc8242f5b8d41dac06d83c0
Cody <codyn329@users.noreply.github.com>
2015-08-28 21:01:26 -0500

    adding George and Jack to authors list

--- Material.php
+++ Material.php
@@ -4,7 +4,7 @@
  *
  * @file
  * @ingroup Skins
- * @author Codyn329, MtMNC
+ * @author Codyn329, MtMNC, George Barnick, Jack Phoenix
  * @version 1.0.0
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 3.0
  */
@@ -20,7 +20,9 @@ $wgExtensionCredits['skin'][] = array(
 	'version' => '1.0.0',
 	'author' => array(
 		'[http://meta.brickimedia.org/wiki/User:Codyn329 Codyn329]',
-		'[http://meta.brickimedia.org/wiki/User:MtMNC MtMNC]'
+		'[http://meta.brickimedia.org/wiki/User:MtMNC MtMNC]',
+		'[http://meta.brickimedia.org/wiki/User:ToaMeiko George Barnick]',
+		'[http://meta.brickimedia.org/wiki/User:Jack_Phoenix Jack Phoenix]'
 	),
 	'descriptionmsg' => 'material-desc',
 	'license' => 'GPL-3.0',
