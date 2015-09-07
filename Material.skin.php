07eaed2a392cd46f41254156861da29273c14cc1
Cody <codyn329@users.noreply.github.com>
2015-09-06 22:42:04 -0500

    adding site wrapper

--- Material.skin.php
+++ Material.skin.php
@@ -36,156 +36,158 @@ class MaterialTemplate extends BaseTemplate {
 
 		$this->html( 'headelement' );
 		?>
-		<header id="mw-header">
-			<a href="<?php echo htmlspecialchars( $this->data['nav_urls']['mainpage']['href'] ); ?>" <?php echo Xml::expandAttributes( Linker::tooltipAndAccesskeyAttribs( 'mw-logo-link' ) ) ?>> <!-- logo link -->
-				<img id="mw-logo-image" role="banner" src="<?php $this->text( 'logopath' ); ?>" alt="<?php $this->text( 'sitename' ) ?>" />
-			</a>
-			<form class="mw-search" id="searchform" action="<?php $this->text( 'wgScript' ); ?>">
-				<input type="hidden" name="title" value="<?php $this->text( 'searchtitle' ) ?>" />
-				<?php
-					echo $this->makeSearchInput( array( 'class' => 'mw-search-input', 'placeholder' => 'Search the Wiki' ) );
-					echo $this->makeSearchButton( 'image', array( 'src' => $this->getSkin()->getSkinStylePath( 'images/magnifying-glass.svg' ), 'alt' => 'search button' ) );
-				?>
-			</form>
-			<?php // language links
-				if ( $this->data['language_urls'] ) {
-					echo '<ul id="mw-language-links" role="menu">';
-					foreach ( $this->data['language_urls'] as $key => $langLink ) {
-						echo $this->makeListItem( $key, $langLink );
+		<div class="wrapper">
+			<header id="mw-header">
+				<a href="<?php echo htmlspecialchars( $this->data['nav_urls']['mainpage']['href'] ); ?>" <?php echo Xml::expandAttributes( Linker::tooltipAndAccesskeyAttribs( 'mw-logo-link' ) ) ?>> <!-- logo link -->
+					<img id="mw-logo-image" role="banner" src="<?php $this->text( 'logopath' ); ?>" alt="<?php $this->text( 'sitename' ) ?>" />
+				</a>
+				<form class="mw-search" id="searchform" action="<?php $this->text( 'wgScript' ); ?>">
+					<input type="hidden" name="title" value="<?php $this->text( 'searchtitle' ) ?>" />
+					<?php
+						echo $this->makeSearchInput( array( 'class' => 'mw-search-input', 'placeholder' => 'Search the Wiki' ) );
+						echo $this->makeSearchButton( 'image', array( 'src' => $this->getSkin()->getSkinStylePath( 'images/magnifying-glass.svg' ), 'alt' => 'search button' ) );
+					?>
+				</form>
+				<?php // language links
+					if ( $this->data['language_urls'] ) {
+						echo '<ul id="mw-language-links" role="menu">';
+						foreach ( $this->data['language_urls'] as $key => $langLink ) {
+							echo $this->makeListItem( $key, $langLink );
+						}
+						echo '</ul>';
 					}
-					echo '</ul>';
-				}
-			?>
-			<nav id="nav-user">
-				<span class="username-wrapper">
-					<?php
-						$materialUsername = htmlspecialchars($this->getSkin()->getUser()->getName(), ENT_QUOTES);
-						$materialGuest = $this->getSkin()->msg( 'material-guest' )->text();
-
-						if ( class_exists( 'wAvatar' ) && $this->data['loggedin'] ) { //socialProfile:T and logged in:T
-							$materialSPAvatar = new wAvatar( $user->getId(), 'l' );
-							$materialSPAvatarImg = $materialSPAvatar->getAvatarURL( array( 'height' => ' 40px', 'class' => 'avatar-img', 'alt' => 'my avatar') );
-							?>
-							<span class="avatar"><?php echo $materialSPAvatar ?></span>
-							<span class="username"><?php echo $materialUsername ?></span>
+				?>
+				<nav id="nav-user">
+					<span class="username-wrapper">
 						<?php
-						} elseif ( class_exists( 'wAvatar' ) && !$this->data['loggedin'] ) { //socialProfile:T and logged in:F
-							?>
-							<span class="avatar"><i class="material-icons">avatar circle</i></span>
-							<span class="username"><?php echo $materialGuest ?></span>
-						<?php
-						} elseif ( !class_exists( 'wAvatar' ) && $this->data['loggedin'] ) { //socialProfile:F and logged in:T
-							?>
-							<span class="avatar"><i class="material-icons">avatar circle</i></span>
-							<span class="username"><?php echo $materialUsername ?></span>
-						<?php
-						} elseif ( !class_exists( 'wAvatar' ) && !$this->data['loggedin'] ) { //socialProfile:F and logged in:F
-							?>
-							<span class="avatar"><i class="material-icons">avatar circle</i></span>
-							<span class="username"><?php echo $materialGuest ?></span>
-						<?php
-						}
-					?>
-				</span>
-				<ul>
-					<?php
-						foreach ( $this->getPersonalTools() as $key => $item ) {
-							echo $this->makeListItem( $key, $item );
-						}
-					?>
-				</ul>
-			</nav>
-		</header>
-		<aside id="nav-menu">
-			<span class="menu" aria-label="menu button"><!-- some icon for a menu --></span>
-			<?php foreach ( $this->getSidebar() as $boxName => $box ) { ?>
-				<section id="<?php echo Sanitizer::escapeId( $box['id'] ) ?>" class="sidebar-group" <?php echo Linker::tooltip( $box['id'] ) ?>>
-				<?php
-					if ( is_array( $box['content'] ) ) { ?>
-						<h2 class="sidebar-header"><?php echo $boxName ?></h2>
-						<ul>
+							$materialUsername = htmlspecialchars($this->getSkin()->getUser()->getName(), ENT_QUOTES);
+							$materialGuest = $this->getSkin()->msg( 'material-guest' )->text();
+	
+							if ( class_exists( 'wAvatar' ) && $this->data['loggedin'] ) { //socialProfile:T and logged in:T
+								$materialSPAvatar = new wAvatar( $user->getId(), 'l' );
+								$materialSPAvatarImg = $materialSPAvatar->getAvatarURL( array( 'height' => ' 40px', 'class' => 'avatar-img', 'alt' => 'my avatar') );
+								?>
+								<span class="avatar"><?php echo $materialSPAvatar ?></span>
+								<span class="username"><?php echo $materialUsername ?></span>
 							<?php
-							foreach ( $box['content'] as $key => $item ) {
-								echo $this->makeListItem( $key, $item );
-							} ?>
-						</ul>
-					<?php
-					} else {
-						echo $box['content'];
-					} ?>
-				</section>
-				<?php
-				} 
-			?>
-		</aside>
-		<?php if ( $this->data['newtalk'] && !class_exists( 'wAvatar' )) { 
-			//check if newtalk notification exists, and if socialProfile doesn't exist 
-			?>
-			<div class="new-talk md-tile" role="dialog" aria-live="polite">
-				<?php $this->html( 'newtalk' ); ?>
-			</div>
-			<?php
-			} 
-		?>
-		<?php if ( $this->data['sitenotice'] ) { ?>
-			<div id="site-notice" class="md-tile" role="alert" aria-live="polite">
-				<?php $this->html( 'sitenotice' ); ?>
-			</div>
-		<?php } ?>
-		<article class="mw-body-content md-tile" role="main">
-			<?php if ( $this->data['title'] != '' ) { ?>
-				<section id="title-section">
-					<?php // page status indicators
-						$oldVersion = version_compare( $wgVersion, '1.25', '<=' );
-						if ( $oldVersion ) {
-							if ( method_exists( $this, 'getIndicators' ) ) {
-								echo $this->getIndicators();
+							} elseif ( class_exists( 'wAvatar' ) && !$this->data['loggedin'] ) { //socialProfile:T and logged in:F
+								?>
+								<span class="avatar"><i class="material-icons">avatar circle</i></span>
+								<span class="username"><?php echo $materialGuest ?></span>
+							<?php
+							} elseif ( !class_exists( 'wAvatar' ) && $this->data['loggedin'] ) { //socialProfile:F and logged in:T
+								?>
+								<span class="avatar"><i class="material-icons">avatar circle</i></span>
+								<span class="username"><?php echo $materialUsername ?></span>
+							<?php
+							} elseif ( !class_exists( 'wAvatar' ) && !$this->data['loggedin'] ) { //socialProfile:F and logged in:F
+								?>
+								<span class="avatar"><i class="material-icons">avatar circle</i></span>
+								<span class="username"><?php echo $materialGuest ?></span>
+							<?php
 							}
-						}
-					?>
-					<h1 class="first-heading"><?php $this->html( 'title' ); ?></h1>
-					<?php
-						if( isset( $_GET["printable"] ) && trim( $_GET["printable"] ) === 'yes' ){
-							?><div id="site-sub"><?php $this->msg( 'tagline' ); ?></div>
+						?>
+					</span>
+					<ul>
 						<?php
-						}
-					?>
-					<?php if ( $this->data['subtitle'] || $this->data['undelete'] ) { ?>
-						<div id="content-sub">
-							<?php $this->html( 'subtitle' ); $this->html( 'undelete' ); ?>
-						</div>
-					<?php } ?>
-					<ul class="content-tabs">
-						<?php
-							foreach ( $this->data['content_navigation'] as $category => $tabs ) {
-								foreach ( $tabs as $key => $tab ) {
-									echo $this->makeListItem( $key, $tab );
-								}
+							foreach ( $this->getPersonalTools() as $key => $item ) {
+								echo $this->makeListItem( $key, $item );
 							}
 						?>
 					</ul>
-				</section>
-			<?php } ?>
-
-			<?php $this->html( 'bodytext' ) ?>
-			<?php
-				$this->html( 'catlinks' );
-				if ( $this->data['dataAfterContent'] ) {
-					$this->html( 'dataAfterContent' );
-				}
-			?>
-		</article>
-		<footer id="mw-footer">
-			<?php foreach ( $this->getFooterLinks() as $category => $links ) { ?>
-				<nav class="nav-footer">
-					<ul>
-					<?php foreach ( $links as $key ) { ?>
-						<li><?php $this->html( $key ) ?></li>
-					<?php } ?>
-					</ul>
 				</nav>
+			</header>
+			<aside id="nav-menu">
+				<span class="menu" aria-label="menu button"><!-- some icon for a menu --></span>
+				<?php foreach ( $this->getSidebar() as $boxName => $box ) { ?>
+					<section id="<?php echo Sanitizer::escapeId( $box['id'] ) ?>" class="sidebar-group" <?php echo Linker::tooltip( $box['id'] ) ?>>
+					<?php
+						if ( is_array( $box['content'] ) ) { ?>
+							<h2 class="sidebar-header"><?php echo $boxName ?></h2>
+							<ul>
+								<?php
+								foreach ( $box['content'] as $key => $item ) {
+									echo $this->makeListItem( $key, $item );
+								} ?>
+							</ul>
+						<?php
+						} else {
+							echo $box['content'];
+						} ?>
+					</section>
+					<?php
+					} 
+				?>
+			</aside>
+			<?php if ( $this->data['newtalk'] && !class_exists( 'wAvatar' )) { 
+				//check if newtalk notification exists, and if socialProfile doesn't exist 
+				?>
+				<div class="new-talk md-tile" role="dialog" aria-live="polite">
+					<?php $this->html( 'newtalk' ); ?>
+				</div>
+				<?php
+				} 
+			?>
+			<?php if ( $this->data['sitenotice'] ) { ?>
+				<div id="site-notice" class="md-tile" role="alert" aria-live="polite">
+					<?php $this->html( 'sitenotice' ); ?>
+				</div>
 			<?php } ?>
-		</footer>
+			<article class="mw-body-content md-tile" role="main">
+				<?php if ( $this->data['title'] != '' ) { ?>
+					<section id="title-section">
+						<?php // page status indicators
+							$oldVersion = version_compare( $wgVersion, '1.25', '<=' );
+							if ( $oldVersion ) {
+								if ( method_exists( $this, 'getIndicators' ) ) {
+									echo $this->getIndicators();
+								}
+							}
+						?>
+						<h1 class="first-heading"><?php $this->html( 'title' ); ?></h1>
+						<?php
+							if( isset( $_GET["printable"] ) && trim( $_GET["printable"] ) === 'yes' ){
+								?><div id="site-sub"><?php $this->msg( 'tagline' ); ?></div>
+							<?php
+							}
+						?>
+						<?php if ( $this->data['subtitle'] || $this->data['undelete'] ) { ?>
+							<div id="content-sub">
+								<?php $this->html( 'subtitle' ); $this->html( 'undelete' ); ?>
+							</div>
+						<?php } ?>
+						<ul class="content-tabs">
+							<?php
+								foreach ( $this->data['content_navigation'] as $category => $tabs ) {
+									foreach ( $tabs as $key => $tab ) {
+										echo $this->makeListItem( $key, $tab );
+									}
+								}
+							?>
+						</ul>
+					</section>
+				<?php } ?>
+	
+				<?php $this->html( 'bodytext' ) ?>
+				<?php
+					$this->html( 'catlinks' );
+					if ( $this->data['dataAfterContent'] ) {
+						$this->html( 'dataAfterContent' );
+					}
+				?>
+			</article>
+			<footer id="mw-footer">
+				<?php foreach ( $this->getFooterLinks() as $category => $links ) { ?>
+					<nav class="nav-footer">
+						<ul>
+						<?php foreach ( $links as $key ) { ?>
+							<li><?php $this->html( $key ) ?></li>
+						<?php } ?>
+						</ul>
+					</nav>
+				<?php } ?>
+			</footer>
+		</div>
 		<?php
 		$this->printTrail();
 		echo Html::closeElement( 'body' );
